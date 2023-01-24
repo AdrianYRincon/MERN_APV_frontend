c1fb5cff2b8937761ab69061fccafab77c18cca3
Adrian yevith Rincon <adriancho60@gmail.com>
2023-01-24 13:06:19 -0500

    deployment

---
+++ src/paginas/EditarPerfil.jsx
@@ -0,0 +1,106 @@
+import { AdminNav } from "../components/AdminNav"
+import useAuth from "../hooks/useAuth"
+import { useEffect, useState } from "react"
+import Alerta from '../components/Alerta'
+
+const EditarPerfil = () => {
+
+  const { auth, actualizarPerfil} = useAuth()
+
+  const [perfil, setPerfil] = useState({})
+
+  const [alerta, setAlerta] = useState({})
+
+  
+  useEffect(() => {
+    setPerfil(auth)
+  },[auth])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const { nombre, email } = perfil
+    if([nombre,email].includes('')){
+      setAlerta({ msg : 'Email y Nombre son obligatorios',
+                  error:true})
+                  return
+    }
+
+    const resultado =  await actualizarPerfil(perfil)
+
+    setAlerta(resultado)
+
+  } 
+  
+  const { msg } = alerta
+
+  return (
+    <>
+    
+    <AdminNav/>
+      <h2 className="font-black text-3xl text-center mt-10">Editar Perfil</h2>
+      <p className="text-xl mt-5 mb-10 text-center">Modifica tu <span className="text-indigo-600 font-bold">Información aqui</span> </p>
+
+      <div className="flex justify-center">
+
+        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+
+          { msg && <Alerta alerta={alerta}/>}
+          <form onSubmit={handleSubmit}>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Nombre</label>
+              <input type='text'
+                     className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                     name='nombre'
+                     value={perfil.nombre || ''}
+                    //  aqui sacamos una copia de lo que hay en el objeto de perfil y despues modificamos el atributo de nombre
+                     onChange={ e => setPerfil({...perfil,[e.target.name]:e.target.value}) }/>
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Sitio Web</label>
+              <input type='text'
+                     className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                     name='web'
+                     value={perfil.web || ''}
+                    //  aqui sacamos una copia de lo que hay en el objeto de perfil y despues modificamos el atributo de web
+                     onChange={ e => setPerfil({...perfil,[e.target.name]:e.target.value}) }/>
+                     
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Teléfono</label>
+              <input type='text'
+                     className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                     name='telefono'
+                     value={perfil.telefono || ''}
+                    //  aqui sacamos una copia de lo que hay en el objeto de perfil y despues modificamos el atributo de telefono
+                     onChange={ e => setPerfil({...perfil,[e.target.name]:e.target.value}) }/>
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Email</label>
+              <input type='text'
+                     className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                     name='email'
+                     value={perfil.email || ''}
+                    //  aqui sacamos una copia de lo que hay en el objeto de perfil y despues modificamos el atributo de email
+                     onChange={ e => setPerfil({...perfil,[e.target.name]:e.target.value}) }/>
+            </div>
+
+            <input type='submit'
+                   value='Guardar Cambios'
+                   className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5 hover:cursor-pointer"
+            />
+
+          </form>
+        
+        </div>
+      
+      </div>
+    </>
+    
+  )
+}
+
+export default EditarPerfil
